3a4ffc8b725446cd6224a6bed1b3bc4236139ca0
Will Clardy <will@quexxon.net>
2015-03-12 14:32:25 -0400

    Matching CSS with original site

--- header.php
+++ header.php
@@ -17,20 +17,37 @@
 <?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?>>
+    <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
-	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'bimg-base' ); ?></a>
+    <a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'bimg-base' ); ?></a>
 
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-		</div><!-- .site-branding -->
+    <header id="masthead" class="site-header" role="banner">
+        <div itemscope itemtype="http://schema.org/Organization" id="site-contact">
+            <span itemprop="name" class="display-none">Christmas Decor</span>Call Us Today!<span itemprop="telephone"></span><span itemprop="email"><?php bloginfo( 'admin_email' ) ?></span>
+        </div>
+        <div id="header-wrapper">
+            <div id="branding-wrapper">
+                <div class="site-branding">
+                <a class="site-title" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><img class="logo" src="<?php echo get_template_directory_uri() . '/images/logo.png'; ?>"></a>
+                </div><!-- .site-branding -->
+                <div class="social-icons">
+                </div><!-- .social-icons -->
+            </div>
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="menu" aria-expanded="false"><?php _e( 'Primary Menu', 'bimg-base' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+            <nav id="site-navigation" class="main-navigation" role="navigation">
+                <button class="menu-toggle" aria-controls="menu" aria-expanded="false"><?php _e( 'Primary Menu', 'bimg-base' ); ?></button>
+                <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+            </nav><!-- #site-navigation -->
+        </div>
+    </header><!-- #masthead -->
+    <header id="sticky-header" class="site-header">
+        <div class"site-branding">
+        </div><!-- .site-branding -->
+        <nav id="site-navigation" class="main-navigation" role="navigation">
+            <button class="menu-toggle" aria-controls="menu" aria-expanded="false"><?php _e( 'Primary Menu', 'bimg-base' ); ?></button>
+            <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+        </nav><!-- #site-navigation -->
+    </header>
 
-	<div id="content" class="site-content">
+    <div id="content" class="site-content">
+
