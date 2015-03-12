f5483b0c85e7662613b02b599ab0f71a5abb4f0c
Will Clardy <will@quexxon.net>
2015-03-12 14:39:48 -0400

    Removed unnecessary template cruft

--- content-page.php
+++ content-page.php
@@ -22,6 +22,5 @@
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
-		<?php edit_post_link( __( 'Edit', 'bimg-base' ), '<span class="edit-link">', '</span>' ); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
